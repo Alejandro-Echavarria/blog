1f195b26d908fb2813bab3900cf934fd250e5e3f
Manuel Alejandro Echavarría Taveras <manuelalejandroechavarriataveras@Manuels-MacBook-Air.local>
2022-11-16 22:02:22 -0400

    estandarizacion de la aplicacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="theme-color" content="#000000">
+        <meta name="author" content="Manuel Echavarria">
+        <title>{{ config('app.name', 'Blog') }}</title>
+        <meta name="description" content="Blog personal."/>
+        <link rel="shortcut icon" href="{{ asset('img/11.ico') }}">
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <!-- Styles -->
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -3,91 +3,75 @@
     $decoradorVertical = '<div class="absolute inset-y-0 top-3"><div class="h-6 border-l-4 border-color-secundario rounded-md"></div></div>';
 @endphp
 
-<nav class="color-primario" x-data="{ open: false }">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-            <div class="flex items-center">
-                {{-- logotipo --}}
-                <a href="/" class="flex-shrink-0">
-                    <p class="font-bold text-gray-200"><span class="color-logo text-3xl">M</span> - dev</p>
-                    {{-- <img class="h-12 w-12" src="{{asset('img/logo-final.svg')}}" alt="Logo representativo"> --}}
-                </a>
-                {{-- Menu lg --}}
-                <div class="hidden md:block relative ml-10 ">
-                    <div class="items-baseline">
-                        <a href="{{route('posts.index')}}" class="text-gray-50 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold">Inicio</a>
-                        {!!request()->routeIs('posts.index') ? $decorador : ""!!}
+<nav class="backdrop-blur-md bg-black/80 navSticky z-50 sticky top-0" x-data="{ open: false }">
+    <div class="max-w-full mx-auto px-6 sm:px-6 lg:px-6 xl:px-8 2xl:px-16">
+        <div class="flex items-center h-16">
+            {{-- logotipo --}}
+            <a href="/" class="flex-shrink-0">
+                <div class="flex items-center">
+                    <div class="items-center">
+                        <p class="font-bold text-gray-200"><span class="color-logo h-12 w-12">M</span> - dev</p>
                     </div>
                 </div>
-            </div>
-            <div class="hidden md:block">
-                <div class="ml-4 flex items-center md:ml-6">
-                    <!-- Profile dropdown -->
-                    @auth
-                        <div class="ml-3 relative" x-data="{ open: false }">
-                            <div>
-                                <button x-on:click="open = ! open" type="button"
-                                    class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span class="sr-only">Open user menu</span>
-                                    <img class="h-8 w-8 rounded-full"
-                                        src="{{ auth()->user()->profile_photo_url }}"
-                                        alt="">
-                                </button>
-                            </div>
-                            <div x-show="open" x-on:click.away="open = false" x-transition:enter.duration.200ms x-transition:leave.duration.200ms style="display: none;" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
-                                tabindex="-1">
-                                
-                                <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
-                                    id="user-menu-item-0">Perfil
-                                </a>
-                                
-                                @can('admin.home')
-                                    <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
-                                        id="user-menu-item-1">Dashboard
-                                    </a>
-                                @endcan
-
-                                <form method="POST" action="{{ route('logout') }}" x-data>
-                                    @csrf    
-                                    <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-2" @click.prevent="$root.submit();">
-                                        Cerrar sesión
-                                    </a>
-                                </form>
-                            </div>
+            </a>
+            <div class="flex justify-end w-full">
+                <div class="flex items-center">
+                    {{-- Menu lg --}}
+                    <div class="hidden md:block relative ml-10 ">
+                        <div class="items-baseline">
+                            <a 
+                                href="{{ route('posts.index') }}" 
+                                class="text-white text-gray-{!!request()->routeIs('posts.index') ? "100" : "300"!!} hover:text-white font-semibold mx-3 py-2 rounded-2xl text-sm transition ease-in-out">Inicio</a>
+                            {!!request()->routeIs('posts.index') ? $decorador : ""!!}
                         </div>
-                    @else
-                        {{-- <a href="{{ route('login') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Login
-                        </a>
-                        <a href="{{ route('register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Register
-                        </a> --}}
-                    @endauth
+                    </div>
+                </div>
+                <div class="hidden md:block">
+                    <div class="ml-4 flex items-center md:ml-6">
+                        <!-- Profile dropdown -->
+                        @auth
+                            <div class="ml-3 relative" x-data="{ open: false }">
+                                <div>
+                                    <button x-on:click="open = ! open" type="button"
+                                        class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                        id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                        <span class="sr-only">Open user menu</span>
+                                        <img class="h-8 w-8 rounded-full"
+                                            src="{{ auth()->user()->profile_photo_url }}"
+                                            alt="">
+                                    </button>
+                                </div>
+                                <div x-show="open" x-on:click.away="open = false" x-transition:enter.duration.200ms x-transition:leave.duration.200ms style="display: none;" class="origin-top-right absolute right-0 mt-2 w-48 rounded-2xl shadow-sm bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                                    tabindex="-1">
+                                    
+                                    <a href="{{ route('profile.show') }}" class="block px-4 m-2 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100 {!! request()->routeIs('profile.show') ? "bg-gray-100" : "" !!} rounded-lg" role="menuitem" tabindex="-1"
+                                        id="user-menu-item-0">Perfil
+                                    </a>
+    
+                                    <form method="POST" action="{{ route('logout') }}" x-data>
+                                        @csrf    
+                                        <a href="{{ route('logout') }}" class="block px-4 m-2 py-2 text-sm text-gray-700 font-semibold hover:bg-gray-100 hover:rounded-lg" role="menuitem" tabindex="-1" id="user-menu-item-2" @click.prevent="$root.submit();">
+                                            Cerrar sesión
+                                        </a>
+                                    </form>
+                                </div>
+                            </div>
+                        @endauth
+                    </div>
                 </div>
             </div>
             
             <!-- Mobile menu button -->
             <div class="-mr-2 flex md:hidden">
                 <button x-on:click="open = true" type="button"
-                    class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    class=" inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     aria-controls="mobile-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
-                    <!--
-                        Heroicon name: outline/menu
-        
-                        Menu open: "hidden", Menu closed: "block"
-                    -->
                     <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
-                    <!--
-                        Heroicon name: outline/x
-        
-                        Menu open: "block", Menu closed: "hidden"
-                    -->
                     <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -99,10 +83,6 @@
     
     <!-- Mobile menu, show/hide based on menu state. -->
     <div class="md:hidden" id="mobile-menu" x-show="open" x-on:click.away="open = false" style="display: none;" x-transition:enter.duration.400ms x-transition:leave.duration.200ms>
-        <div class="px-4 pt-2 pb-3 space-y-1 relative">
-            <a href="{{route('posts.index')}}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold">Inicio</a>
-            {!!request()->routeIs('posts.index') ? $decoradorVertical : ""!!}
-        </div>
         @auth
             <div class="pt-4 pb-3 border-t border-gray-700">
                 <div class="flex items-center px-4">
@@ -118,16 +98,13 @@
                 </div>
                 <div class="mt-3">
                     <div class="px-4 pt-2 space-y-1 relative">
-                        <a href="{{ route('profile.show') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold">Perfil</a>
+                        <a href="{{ route('profile.show') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-sm transition ease-in-out">Perfil</a>
                         {!!request()->routeIs('profile.show') ? $decoradorVertical : ""!!}
-                    </div>
-                    <div class="px-4 pt-2 space-y-1 relative">
-                        <a href="{{ route('admin.home') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold">Dashboard</a>
                     </div>
                     <div class="px-4 pt-2 space-y-1 relative">
                         <form method="POST" action="{{ route('logout') }}" x-data>
                             @csrf   
-                            <a href="{{ route('logout') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold" @click.prevent="$root.submit();">
+                            <a href="{{ route('logout') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-sm transition ease-in-out" @click.prevent="$root.submit();">
                                 Cerrar sesión
                             </a>
                         </form>
@@ -135,10 +112,9 @@
                 </div>
             </div>
         @else
-            {{-- <div class="px-4 pt-2 pb-3 space-y-1 sm:px-3 border-t border-gray-700">
-                <a href="{{ route('login') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold">Login</a>
-                <a href="{{ route('register') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-bold">Register</a>
-            </div> --}}
+            <div class="px-4 pt-2 pb-3 space-y-1 sm:px-3 border-t border-gray-700">
+                <a href="{{ route('login') }}" class="text-gray-50 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-sm transition ease-in-out">Iniciar</a>
+            </div>
         @endauth
     </div>
 </nav>
